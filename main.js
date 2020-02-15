22b86738673d76b287a45b3c2a75982680af0d55
Valmnt <valentin.mont@ynov.com>
2020-02-15 20:41:10 +0100

    fix code

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const menu = electron.Menu;
-const Notification = electron.Notification;
 
 var win = null;
 
 // Main App
 app.on('ready', function () {
-    win = new BrowserWindow({ width: 1920, height: 1080, frame: true, resizable: true, fullscreen: true, icon: __dirname + 'views/assets/icon.png' });
+    win = new BrowserWindow({ width: 1920, height: 1080, frame: true, resizable: true, fullscreen: true });
 
     const template = [
         {
