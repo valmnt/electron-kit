906d458ca96798f686f3aff2aaa699b38e82e1ba
Valmnt <valentin.mont@ynov.com>
2020-02-16 18:32:43 +0100

    v 0.0.2

--- main.js
+++ main.js
@@ -2,12 +2,51 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const menu = electron.Menu;
+const MenuItem = electron.MenuItem;
+const { ipcMain } = require('electron');
 
 var win = null;
 
 // Main App
 app.on('ready', function () {
-    win = new BrowserWindow({ width: 1920, height: 1080, frame: true, resizable: true, fullscreen: true });
+    win = new BrowserWindow({
+        width: 1920, height: 1080, frame: true, resizable: true, fullscreen: true, webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    ipcMain.on('asynchronous-message', (event, arg) => {
+        if (arg == "") {
+            arg = 'Guest'
+        }
+        const template = [
+            {
+                label: 'Navigation',
+                submenu: [
+                    {
+                        label: 'Accueil',
+                        click: function () {
+                            win.loadURL(`file://${__dirname}/views/index.html`);
+                        }
+                    },
+                ]
+            }
+        ]
+
+        const Menu = menu.buildFromTemplate(template);
+
+        Menu.append(new MenuItem({
+            label: 'Login : ' + arg
+        }))
+
+        Menu.append(new MenuItem({
+            label: 'Exit',
+            accelerator: 'esc',
+            click: () => { win.setProgressBar(1), app.quit() }
+        }));
+
+        menu.setApplicationMenu(Menu)
+    });
 
     const template = [
         {
@@ -16,20 +55,35 @@ app.on('ready', function () {
                 {
                     label: 'Accueil',
                     click: function () {
-                        win.loadURL(`file://${__dirname}/views/index.html`);
+                        win.loadURL(`file://${__dirname}/views/home.html`);
                     }
                 },
-                {
-                    label: 'Exit',
-                    click: function () {
-                        app.quit();
-                    }
-                }
             ]
         }
     ]
+
     const Menu = menu.buildFromTemplate(template);
     menu.setApplicationMenu(Menu)
-    win.loadFile('views/index.html');
+
+    win.loadFile('views/home.html');
 });
+
+// Option App
+app.setUserTasks([
+    {
+        program: process.execPath,
+        arguments: '--new-window',
+        iconPath: process.execPath,
+        iconIndex: 0,
+        title: 'Nouvelle fenêtre',
+        description: 'Créer une nouvelle fenêtre'
+    },
+])
+
+// App Quit
+app.on('quit', function () {
+    console.log('App Down');
+})
+
+
 
